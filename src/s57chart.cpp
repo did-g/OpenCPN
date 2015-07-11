363e26cfae99229191b20732814d996ff758aa8b
didier <dgautheron@magic.fr>
2015-07-11 02:17:10 +0200

    stash 3

--- src/s57chart.cpp
+++ src/s57chart.cpp
@@ -306,6 +306,7 @@ S57Obj::S57Obj( char *first_line, wxInputStream *pfpx, double dummy, double dumm
 
     int FEIndex;
 
+    //char *buf = (char *) malloc( MAX_LINE + 1 );
     char buf[MAX_LINE +1];
     int llmax = 0;
 
@@ -626,8 +627,12 @@ S57Obj::S57Obj( char *first_line, wxInputStream *pfpx, double dummy, double dumm
 
                         my_fgets( buf, MAX_LINE, *pfpx );
                         int sb_len = atoi( buf + 2 );
-
-                        unsigned char *buft = (unsigned char *) malloc( sb_len );
+                        unsigned char *buft;
+                        if (sb_len > MAX_LINE) 
+                            buft = (unsigned char *) malloc( sb_len );
+                        else
+                            buft = (unsigned char *) buf;
+                        // unsigned char buft[ sb_len ];
                         pfpx->Read( buft, sb_len );
 
                         npt = *( (int *) ( buft + 5 ) );
@@ -667,7 +672,8 @@ S57Obj::S57Obj( char *first_line, wxInputStream *pfpx, double dummy, double dumm
                         ymax = *pf++;
                         ymin = *pf;
 #endif
-                        free( buft );
+                        if (sb_len > MAX_LINE) 
+                            free( buft );
 
                         // set s57obj bbox as lat/lon
                         BBObj.SetMin( xmin, ymin );
@@ -730,11 +736,17 @@ S57Obj::S57Obj( char *first_line, wxInputStream *pfpx, double dummy, double dumm
                         sscanf( buf, " %s %d %f %f", tbuf, &nrecl, &area_ref_lat, &area_ref_lon );
 
                         if( nrecl ) {
-                            unsigned char *polybuf = (unsigned char *) malloc( nrecl + 1 );
+                            unsigned char *polybuf;
+                            if (nrecl > MAX_LINE)
+                                polybuf = (unsigned char *) malloc( nrecl + 1 );
+                            else
+                                polybuf = (unsigned char *)buf;
+                            // unsigned char polybuf [nrecl + 1];
                             pfpx->Read( polybuf, nrecl );
                             polybuf[nrecl] = 0;                     // endit
                             PolyTessGeo *ppg = new PolyTessGeo( polybuf, nrecl, FEIndex, senc_file_version );
-                            free( polybuf );
+                            if (nrecl > MAX_LINE)
+                                free( polybuf );
 
                             pPolyTessGeo = ppg;
 
@@ -787,6 +799,7 @@ S57Obj::S57Obj( char *first_line, wxInputStream *pfpx, double dummy, double dumm
         }               //OGRF
     }                       //while(!dun)
 
+    //free( buf );
     free( hdr_buf );
 
 }
