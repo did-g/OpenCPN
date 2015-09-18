fa6a6877aa5fe1c023f24b4202ff27fe9c102671
didier <dgautheron@magic.fr>
2015-09-18 20:26:38 +0200

    Revert "remove unrelated g_tex_count declaration"
    This reverts commit 095cf2df3342f5559a6daa2ce71c54ec41dd5a6f.
    But WIP_next needs it...

--- src/glChartCanvas.cpp
+++ src/glChartCanvas.cpp
@@ -198,6 +198,7 @@ PFNGLDELETEBUFFERSPROC              s_glDeleteBuffers;
 
 GLuint g_raster_format = GL_RGB;
 long g_tex_mem_used;
+int  g_tex_count;
 
 bool            b_timeGL;
 wxStopWatch     g_glstopwatch;
