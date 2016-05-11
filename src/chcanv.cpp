92ff3a3557d817f2dd98cbad66b24a4ba0cd4cd5
did-g <did-g@users.noreply.github.com>
2016-05-11 15:35:25 +0200

    test unit add a pause 'p' key

--- src/chcanv.cpp
+++ src/chcanv.cpp
@@ -1696,6 +1696,9 @@ void ChartCanvas::OnKeyDown( wxKeyEvent &event )
             parent_frame->ToggleQuiltMode();
             break;
 
+        case 'P':
+            parent_frame->ToggleTestPause();
+            break;
 #if 0
         case 'R':
             parent_frame->ToggleRocks();
