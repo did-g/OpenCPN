2dc43b745cbc7c56ddfae69ad1d2f4fa6490a6f1
did-g <did-g@users.noreply.github.com>
2016-05-28 04:57:31 +0200

    move g_params out of wxUSE_CMDLINE_PARSER

--- src/chart1.cpp
+++ src/chart1.cpp
@@ -176,10 +176,8 @@ bool                      g_start_fullscreen;
 bool                      g_rebuild_gl_cache;
 bool                      g_parse_all_enc;
 
-#if wxUSE_CMDLINE_PARSER
 // Files specified on the command line, if any.
 wxVector<wxString> g_params;
-#endif
                 
 
 MyFrame                   *gFrame;
@@ -6816,7 +6814,6 @@ void MyFrame::OnInitTimer(wxTimerEvent& event)
             break;
         }
 
-#if wxUSE_CMDLINE_PARSER
         case 5:
         {
             if ( !g_params.empty() ) {
@@ -6837,7 +6834,6 @@ void MyFrame::OnInitTimer(wxTimerEvent& event)
             break;
 
         }
-#endif
         default:
         {
             // Last call....
