2489f7a3edc1027b278d893f8d4533451b102504
did-g <did-g@users.noreply.github.com>
2017-08-30 10:42:52 +0200

    #if 0 dead code

--- src/routeman.cpp
+++ src/routeman.cpp
@@ -1694,6 +1694,7 @@ wxBitmap *WayPointman::GetIconBitmap( const wxString& icon_key )
             break;
     }
 
+#if 0
     if( i == m_pIconArray->GetCount() )              // key not found
     {
         // find and return bitmap for "circle"
@@ -1703,6 +1704,7 @@ wxBitmap *WayPointman::GetIconBitmap( const wxString& icon_key )
 //                break;
         }
     }
+#endif
 
     if( i == m_pIconArray->GetCount() )              // "circle" not found
         pmi = (MarkIcon *) m_pIconArray->Item( 0 );       // use item 0
@@ -1730,7 +1732,8 @@ bool WayPointman::GetIconPrescaled( const wxString& icon_key )
         if( pmi->icon_name.IsSameAs( icon_key ) )
             break;
     }
-    
+
+#if 0
     if( i == m_pIconArray->GetCount() )              // key not found
     {
         // find and return bitmap for "circle"
@@ -1740,6 +1743,7 @@ bool WayPointman::GetIconPrescaled( const wxString& icon_key )
             //                break;
         }
     }
+#endif
     
     if( i == m_pIconArray->GetCount() )              // "circle" not found
         pmi = (MarkIcon *) m_pIconArray->Item( 0 );       // use item 0
