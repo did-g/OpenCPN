137d3c2400ba08df5b0c9602e4da688335a00d11
did-g <did-g@users.noreply.github.com>
2017-08-31 23:42:26 +0200

    cm93 strncpy usage

--- src/cm93.cpp
+++ src/cm93.cpp
@@ -3355,10 +3355,8 @@ S57Obj *cm93chart::CreateS57Obj ( int cell_index, int iobject, int subcell, Obje
 
       pobj->Index = iobject;
 
-      char u[201];
-      strncpy ( u, sclass_sub.mb_str(), 199 );
-      u[200] = '\0';
-      strncpy ( pobj->FeatureName, u, 7 );
+      strncpy ( pobj->FeatureName, sclass_sub.mb_str(), 7 );
+      pobj->FeatureName[7] = 0;
 
       //  Touch up the geom types
       int geomtype_sub = geomtype;
@@ -4471,10 +4469,7 @@ int cm93chart::loadsubcell ( int cellindex, wxChar sub_char )
 
       if ( g_bDebugCM93 )
       {
-            char sfile[200];
-            strncpy ( sfile, file.mb_str(), 199 );
-            sfile[199] = 0;
-            printf ( "    filename: %s\n", sfile );
+            printf ( "    filename: %s\n", ( const char * ) file.mb_str());
       }
 
       wxString compfile;
@@ -4501,10 +4496,7 @@ int cm93chart::loadsubcell ( int cellindex, wxChar sub_char )
 
             if ( g_bDebugCM93 )
             {
-                  char sfile[200];
-                  strncpy ( sfile, file1.mb_str(), 199 );
-                  sfile[199] = 0;
-                  printf ( "    alternate filename: %s\n", sfile );
+                  printf ( "    alternate filename: %s\n", ( const char * ) file1.mb_str() );
             }
 
             if ( !::wxFileExists ( file1 ) ) {
@@ -4548,10 +4540,7 @@ int cm93chart::loadsubcell ( int cellindex, wxChar sub_char )
 
       if ( g_bDebugCM93 )
       {
-            char str[256];
-            strncpy ( str, msg.mb_str(), 255 );
-            str[255] = 0;
-            printf ( "   %s\n", str );
+            printf ( "   %s\n", ( const char * ) msg.mb_str()  );
       }
 
       //    Ingest it
@@ -4690,10 +4679,10 @@ cm93_dictionary *cm93manager::FindAndLoadDict ( const wxString &file )
             }
             i++;
       }
-
+#if 0
       char t[100];
       strncpy ( t, target.mb_str(), 99 );
-
+#endif
       if ( retval == NULL )
             delete pdict;
 
