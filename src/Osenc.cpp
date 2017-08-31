67769f9f8c1e667ae1d5ff610db89ccdd83d8882
did-g <did-g@users.noreply.github.com>
2017-08-31 07:07:01 +0200

    plug some memory leak on error

--- src/Osenc.cpp
+++ src/Osenc.cpp
@@ -1927,19 +1927,21 @@ bool Osenc::CreateMultiPointFeatureGeometryRecord200( OGRFeature *pFeature, Osen
     //  Write the base record
     size_t targetCount = sizeof(record);
     if(!stream->Write(&record, targetCount).IsOk())
-        return false;
-    
+        goto failure;
     //  Write the 3D point array
     targetCount = nPoints * 3 * sizeof(float);
     if(!stream->Write(psb_buffer, targetCount).IsOk())
-        return false;
-        
+        goto failure;
 
     //  Free the buffers
     free( psb_buffer );
     free( pwkb_buffer );
-    
     return true;
+failure:
+    //  Free the buffers
+    free( psb_buffer );
+    free( pwkb_buffer );
+    return false;
 }
 
 
@@ -2964,8 +2966,10 @@ bool Osenc::CreateSENCRecord200( OGRFeature *pFeature, Osenc_outstream *stream, 
                     
                     // Write the record out....
                     size_t targetCount = recordLength;
-                    if(!stream->Write(pBuffer, targetCount).IsOk())
+                    if(!stream->Write(pBuffer, targetCount).IsOk()) {
+                        free( payloadBuffer );
                         return false;
+                    }
                     
                 }
 
@@ -3003,8 +3007,10 @@ bool Osenc::CreateSENCRecord200( OGRFeature *pFeature, Osenc_outstream *stream, 
                     
                 // Write the record out....
                 size_t targetCount = recordLength;
-                if(!stream->Write(pBuffer, targetCount).IsOk())
+                if(!stream->Write(pBuffer, targetCount).IsOk()) {
+                        free( payloadBuffer );
                         return false;
+                }
             }
         }
     }
