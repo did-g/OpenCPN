76d6f63223582abbffc9ef62f85c0b452d0d0410
did-g <did-g@users.noreply.github.com>
2017-10-24 22:06:03 +0200

    use a reference rather than a copy for ocpnDC

--- src/glChartCanvas.cpp
+++ src/glChartCanvas.cpp
@@ -3829,7 +3829,7 @@ void glChartCanvas::Render()
 
 
 
-void glChartCanvas::RenderCanvasBackingChart( ocpnDC dc, OCPNRegion valid_region)
+void glChartCanvas::RenderCanvasBackingChart( ocpnDC &dc, OCPNRegion valid_region)
 {
  
     glPushMatrix();
