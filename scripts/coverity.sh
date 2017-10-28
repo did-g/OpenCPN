1c7a4d1d27ed4155735b45175e35de2c0e682e1b
didier <dgautheron@magic.fr>
2017-10-28 02:21:01 +0200

    gcc 6 again

--- scripts/coverity.sh
+++ scripts/coverity.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make -s
 ls -l 
-head -n 7000 /home/travis/build/did-g/OpenCPN/build_s/cov-int/build-log.txt
+tail -n 1000 /home/travis/build/did-g/OpenCPN/build_s/cov-int/build-log.txt
