d43fe73eb67977b3f98c13799d3590b3b339d930
did-g <did-g@users.noreply.github.com>
2017-11-06 17:52:52 +0100

    assert array index >= 0

--- src/glChartCanvas.cpp
+++ src/glChartCanvas.cpp
@@ -3120,6 +3120,7 @@ void glChartCanvas::RenderNoDTA(ViewPort &vp, ChartBase *chart)
         }
 #else
         int j = pCurrentStack->GetDBIndex(i);
+        assert (j >= 0);
         pt = (ChartTableEntry *) &ChartData->GetChartTableEntry( j );
         if(pt->GetpsFullPath()->IsSameAs(chart->GetFullPath())){
             index = j;
