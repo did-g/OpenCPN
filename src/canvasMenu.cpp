be93c13cc724a3dd6f49ccaad4c9d04e90ff8d36
did-g <did-g@users.noreply.github.com>
2018-04-03 07:25:46 +0200

    display AIS menu first, fs #2366

--- src/canvasMenu.cpp
+++ src/canvasMenu.cpp
@@ -570,7 +570,7 @@ void CanvasMenuHandler::CanvasPopupMenu( int x, int y, int seltype )
         }
     }
 
-    if( seltype & SELTYPE_ROUTESEGMENT ) {
+    if( menuFocus != menuAIS && (seltype & SELTYPE_ROUTESEGMENT) ) {
         bool blay = false;
         if( m_pSelectedRoute && m_pSelectedRoute->m_bIsInLayer )
             blay = true;
@@ -636,7 +636,7 @@ void CanvasMenuHandler::CanvasPopupMenu( int x, int y, int seltype )
         menuFocus = menuRoute;
     }
 
-    if( seltype & SELTYPE_TRACKSEGMENT ) {
+    if( menuFocus != menuAIS && (seltype & SELTYPE_TRACKSEGMENT) ) {
         bool blay = false;
         if( m_pSelectedTrack && m_pSelectedTrack->m_bIsInLayer )
             blay = true;
@@ -656,7 +656,7 @@ void CanvasMenuHandler::CanvasPopupMenu( int x, int y, int seltype )
         menuFocus = menuTrack;
     }
 
-    if( seltype & SELTYPE_ROUTEPOINT ) {
+    if( menuFocus != menuAIS && (seltype & SELTYPE_ROUTEPOINT) ) {
         bool blay = false;
         if( m_pFoundRoutePoint && m_pFoundRoutePoint->m_bIsInLayer )
             blay = true;
@@ -712,7 +712,7 @@ void CanvasMenuHandler::CanvasPopupMenu( int x, int y, int seltype )
         menuFocus = menuWaypoint;
     }
 
-    if( seltype & SELTYPE_MARKPOINT ) {
+    if( menuFocus != menuAIS && (seltype & SELTYPE_MARKPOINT) ) {
         bool blay = false;
         if( m_pFoundRoutePoint && m_pFoundRoutePoint->m_bIsInLayer )
             blay = true;
