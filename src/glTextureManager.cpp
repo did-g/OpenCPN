did-g/OpenCPN

commit d38cc0100ba10025046b2e62ffbeee3a86dade32

Correct OpenGL path processing for portable mode (FS#2141)

src/glTextureManager.cpp
@@ -90,7 +90,8 @@ wxString CompressedCachePath(wxString path)
 {
 #if defined(__WXMSW__)
     int colon = path.find(':', 0);
-    path.Remove(colon, 1);
+    if(colon != wxNOT_FOUND)
+        path.Remove(colon, 1);
 #endif
     
     /* replace path separators with ! */

src/glChartCanvas.cpp
@@ -2497,6 +2497,12 @@ void glChartCanvas::RenderRasterChartRegionGL( ChartBase *chart, ViewPort &vp, L
 
     //    Look for the texture factory for this chart
     wxString key = chart->GetFullPath();
+    
+    /* replace path separators with ! */
+    wxChar separator = wxFileName::GetPathSeparator();
+    for(unsigned int pos = 0; pos < key.size(); pos = key.find(separator, pos))
+        key.replace(pos, 1, _T("!"));
+    
     glTexFactory *pTexFact;
     ChartPathHashTexfactType &hash = g_glTextureManager->m_chart_texfactory_hash;
     ChartPathHashTexfactType::iterator ittf = hash.find( key );

commit 1019829764a202533cfb93156b8bfa18644d81f1

glTextureManager::TextureCrunch use the right string for testing if a chart is quilted
likely broken since d38cc01 Fri Jan 13 13:15:46 2017 -0500
    Correct OpenGL path processing for portable mode (FS#2141)

and was missed in following fix 64b8e62d4

src/glTextureManager.cpp
@@ -1269,10 +1269,10 @@ bool glTextureManager::TextureCrunch(double factor)
     
     ChartPathHashTexfactType::iterator it0;
     for( it0 = m_chart_texfactory_hash.begin(); it0 != m_chart_texfactory_hash.end(); ++it0 ) {
-        wxString chart_full_path = it0->first;
         glTexFactory *ptf = it0->second;
         if(!ptf)
             continue;
+        wxString chart_full_path = ptf->GetChartPath();
         
         bGLMemCrunch = g_tex_mem_used > (double)(g_GLOptions.m_iTextureMemorySize * 1024 * 1024) * factor *hysteresis;
         if(!bGLMemCrunch)