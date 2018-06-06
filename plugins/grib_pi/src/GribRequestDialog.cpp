d443c5b1e67798848ce87cd3cdf9a763f7c3effb
did-g <did-g@users.noreply.github.com>
2018-06-06 13:27:05 +0200

    HRRR sub hourly has no CAPE and clouds in inventory

--- plugins/grib_pi/src/GribRequestDialog.cpp
+++ plugins/grib_pi/src/GribRequestDialog.cpp
@@ -1227,15 +1227,18 @@ wxString GribRequestSetting::WriteMail()
         notfirst = true;
     }
     if ( m_pCloudCover->IsChecked()) {
-        if (notfirst) r_parameters.Append( s[provider]);
-        if (provider == NOAA) {
-            ea = true;
-            r_parameters.Append((model != NAM)?_T("lev_entire_atmosphere=on&")
-                :_T("lev_entire_atmosphere_%%5C%%28considered_as_a_single_layer%%5C%%29=on&")
-            );
+        if (!(provider == NOAA && model == HRRR && m_pInterval->GetStringSelection().Length() != 1)) {
+            // not available in NOAA HRRR sub hourly
+            if (notfirst) r_parameters.Append( s[provider]);
+            if (provider == NOAA) {
+                ea = true;
+                r_parameters.Append((model != NAM)?_T("lev_entire_atmosphere=on&")
+                        :_T("lev_entire_atmosphere_%%5C%%28considered_as_a_single_layer%%5C%%29=on&")
+                    );
+            }
+            r_parameters.Append( p[provider][1]);
+            notfirst = true;
         }
-        r_parameters.Append( p[provider][1]);
-        notfirst = true;
     }
     if ( m_pAirTemp->IsChecked()) {
         if (notfirst) r_parameters.Append( s[provider]);
@@ -1258,9 +1261,11 @@ wxString GribRequestSetting::WriteMail()
         notfirst = true;
     }
     if ( m_pCAPE->IsChecked()) {
-        if (notfirst) r_parameters.Append( s[provider]);
-        r_parameters.Append( p[provider][6]);
-        notfirst = true;
+        if (!(provider == NOAA && model == HRRR && m_pInterval->GetStringSelection().Length() != 1)) {
+            if (notfirst) r_parameters.Append( s[provider]);
+            r_parameters.Append( p[provider][6]);
+            notfirst = true;
+        }
     }
     if ( m_pReflectivity->IsChecked()) {
         if (notfirst) r_parameters.Append( s[provider]);
