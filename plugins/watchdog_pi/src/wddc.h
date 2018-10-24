d5865a320c4a77fe760067a6385e27f2e996ef51
didier <didier@users.sourceforge.net>
2018-10-24 12:06:13 +0200

    XXXX temporary MINGW

--- plugins/watchdog_pi/src/wddc.h
+++ plugins/watchdog_pi/src/wddc.h
@@ -53,15 +53,14 @@
 #ifdef __WXMSW__
 #  define DECL_EXP     __declspec(dllexport)
 #else
+# ifdef __GNUC__
+# define DECL_EXP       __attribute__((visibility("default")))
+#else
 #  define DECL_EXP
+# endif
 #endif
 #endif
 
-
-#ifdef __GNUC__
-#undef  DECL_EXP
-#define DECL_EXP       __attribute__((visibility("default")))
-#endif
 
 void DrawGLThickLine( float x1, float y1, float x2, float y2, wxPen pen, bool b_hiqual );
 
