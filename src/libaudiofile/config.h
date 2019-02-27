c8fb8c801741f7a9ede152fab88c270b9797950a
didier <didier@users.sourceforge.net>
2019-02-27 21:45:38 +0100

    Squashed 'plugins/weatherfax_pi/' content from commit 0b48769cc
    git-subtree-dir: plugins/weatherfax_pi
    git-subtree-split: 0b48769cc28ae649e88342174fe05079719a1282

---
+++ src/libaudiofile/config.h
@@ -0,0 +1,21 @@
+#if defined(__MSVC__)
+#if defined(__MINGW32__)
+  #include <basetsd.h>
+#else
+  #include <BaseTsd.h>
+  typedef int ssize_t;
+  #define snprintf  _snprintf
+  #define HAVE_UNISTD_H 0
+
+  #pragma warning(disable: 4244)
+  #pragma warning(disable: 4800)
+#endif
+
+#else
+  /* Define to 1 if you have the <unistd.h> header file. */
+  #define HAVE_UNISTD_H 1
+#endif
+
+#if (defined(__GNUC__) && defined(__STRICT_ANSI__)) || (defined(_MSC_VER) && !defined(__cplusplus))   /* Visual Studio */
+#  define inline __inline           /* Visual C is not C99, but supports some kind of inline */
+#endif
